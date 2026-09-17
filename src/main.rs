7449ce3cfa53926cfe0cccba0197b26374c5fcc7
Evgeniy Dorovatovskiy <6276019+gitex@users.noreply.github.com>
2026-09-17 15:00:46 +0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,125 @@
+use macroquad::prelude::*;
+use std::{thread, time::Duration};
+
+const BOARD_HEIGHT: usize = 30;
+const BOARD_WIDTH: usize = 30;
+const CELL_SIZE: f32 = 30.;
+
+const AROUND_POSITIONS: [[i8; 2]; 8] = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+];
+
+const INITIAL_CELLS: [[usize; 2]; 5] = [[1, 0], [2, 1], [0, 2], [1, 2], [2, 2]];
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Game of life".to_owned(),
+        window_width: (CELL_SIZE as i32 * BOARD_WIDTH as i32),
+        window_height: (CELL_SIZE as i32 * BOARD_HEIGHT as i32),
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+type Matrix = [[u8; BOARD_WIDTH]; BOARD_HEIGHT];
+
+struct Cell {
+    row: usize,
+    col: usize,
+}
+
+fn get_alive_neighbors(matrix: Matrix, cell: Cell) -> usize {
+    let mut alive: usize = 0;
+
+    for [row, col] in AROUND_POSITIONS {
+        let current_row: i8 = cell.row as i8 + row;
+        let current_col: i8 = cell.col as i8 + col;
+
+        if (current_row < 0) || (current_row as usize >= BOARD_HEIGHT) {
+            continue;
+        };
+
+        if (current_col < 0) || (current_col as usize >= BOARD_WIDTH) {
+            continue;
+        };
+
+        if matrix[current_row as usize][current_col as usize] == 1 {
+            alive += 1;
+        };
+    }
+
+    return alive;
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    let mut board: Matrix = [[0; BOARD_WIDTH]; BOARD_HEIGHT];
+
+    for [row, col] in INITIAL_CELLS {
+        board[BOARD_HEIGHT / 2 + row][BOARD_WIDTH / 2 + col] = 1;
+    }
+
+    let minimum_frame_time = 10. / 2.;
+    let frame_time = get_frame_time();
+
+    loop {
+        clear_background(WHITE);
+
+        for row in 0..BOARD_HEIGHT {
+            for col in 0..BOARD_WIDTH {
+                if board[row][col] == 1 {
+                    draw_rectangle(
+                        row as f32 * CELL_SIZE,
+                        col as f32 * CELL_SIZE,
+                        CELL_SIZE,
+                        CELL_SIZE,
+                        BLACK,
+                    )
+                }
+            }
+        }
+
+        let mut new_board = board.clone();
+
+        for row in 0..BOARD_HEIGHT {
+            for col in 0..BOARD_WIDTH {
+                let cell = Cell { row, col };
+                let alive_neighbors: usize = get_alive_neighbors(board, cell);
+                let current_cell_alive = board[row][col] == 1;
+
+                let text_color = if current_cell_alive { WHITE } else { BLACK };
+
+                if alive_neighbors > 0 {
+                    draw_text(
+                        alive_neighbors.to_string().as_str(),
+                        (row as f32 * CELL_SIZE) + (CELL_SIZE / 3.),
+                        (col as f32 * CELL_SIZE) + (CELL_SIZE / 1.5),
+                        25.0,
+                        text_color,
+                    );
+                }
+
+                if current_cell_alive {
+                    new_board[row][col] = (alive_neighbors == 2 || alive_neighbors == 3) as u8;
+                } else {
+                    new_board[row][col] = (alive_neighbors == 3) as u8;
+                }
+            }
+        }
+        board = new_board;
+
+        next_frame().await;
+
+        if frame_time < minimum_frame_time {
+            let time_to_sleep = (minimum_frame_time - frame_time) * 1000.;
+            std::thread::sleep(std::time::Duration::from_millis(time_to_sleep as u64));
+        }
+    }
+}
